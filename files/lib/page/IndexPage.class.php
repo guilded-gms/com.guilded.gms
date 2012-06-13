02cdf8c5726df52aeee48f75cf1fef6a6947d70f
Kiv4h <jeffrey.reichardt@googlemail.com>
2012-06-13 19:02:23 +0200

    Added index redirection

--- files/lib/page/IndexPage.class.php
+++ files/lib/page/IndexPage.class.php
@@ -1,7 +1,20 @@
 <?php
 namespace gms\page;
 use wcf\page\AbstractPage;
+use wcf\util\FileUtil;
+use wcf\util\HeaderUtil;
 
 class IndexPage extends AbstractPage {
-
+	/**
+	 * @see IPage::show()
+	 */
+	public function show() {
+		//redirecting to another start page
+		if (FileUtil::isURL(INDEX_PAGE_REDIRECT)) {
+			HeaderUtil::redirect(INDEX_PAGE_REDIRECT, false);
+			exit;
+		}
+	
+		parent::show();
+	}
 }
