ed654bfb575162a2c6f2b368d8a911e648f203a2
Kiv4h <jeffrey.reichardt@googlemail.com>
2014-01-08 21:59:28 +0100

    added faction pip

--- files/lib/system/package/plugin/GameFactionPackageInstallationPlugin.class.php
+++ files/lib/system/package/plugin/GameFactionPackageInstallationPlugin.class.php
@@ -6,7 +6,7 @@ use wcf\system\package\plugin\AbstractXMLPackageInstallationPlugin;
 use wcf\system\WCF;
 
 /**
- * Package-installation-plugin implementation for game race.
+ * Package-installation-plugin implementation for game faction.
  *
  * @author	Jeffrey Reichardt
  * @copyright	2012-2014 DevLabor UG (haftungsbeschränkt)
@@ -15,16 +15,16 @@ use wcf\system\WCF;
  * @subpackage	system.package.plugin
  * @category	Guilded 2.0
  */
-class GameRacePackageInstallationPlugin extends AbstractXMLPackageInstallationPlugin {
+class GameFactionPackageInstallationPlugin extends AbstractXMLPackageInstallationPlugin {
 	/**
 	 * @see	\wcf\system\package\plugin\AbstractXMLPackageInstallationPlugin::$className
 	 */
-	public $className = 'gms\data\game\race\GameRaceEditor';
+	public $className = 'gms\data\game\faction\GameFactionEditor';
 	
 	/**
 	 * @see	\wcf\system\package\plugin\AbstractPackageInstallationPlugin::$tableName
 	 */
-	public $tableName = 'game_race';
+	public $tableName = 'game_faction';
 
 	/**
 	 * @see	\wcf\system\package\plugin\AbstractPackageInstallationPlugin::$application
@@ -34,7 +34,7 @@ class GameRacePackageInstallationPlugin extends AbstractXMLPackageInstallationPl
 	/**
 	 * @see	\wcf\system\package\plugin\AbstractPackageInstallationPlugin::$tagName
 	 */
-	public $tagName = 'race';
+	public $tagName = 'faction';
 
 	/**
 	 * game object
@@ -44,8 +44,6 @@ class GameRacePackageInstallationPlugin extends AbstractXMLPackageInstallationPl
 
 	/**
 	 * @see	\wcf\system\package\plugin\AbstractXMLPackageInstallationPlugin::handleDelete()
-	 *
-	 * @todo handle factions
 	 */
 	protected function handleDelete(array $items) {
 		// get game id
